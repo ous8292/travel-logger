9fd5b87c78de49089ea200dbd6fdd5ed20c74528
Peter <peter@Peters-Air.lan>
2022-04-26 17:13:57 -0500

    adding mapbox component and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+// import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const App = () => {
-  const [viewport, setViewport] = useState({
-    width: "100vh",
-    height: "100vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-  });
+// const App = () => {
+//   const [viewport, setViewport] = useState({
+//     width: "100vh",
+//     height: "100vh",
+//     longitude: -122.4,
+//     latitude: 37.8,
+//     zoom: 8,
+//   });
 
+//   return (
+//     <ReactMapGL
+//       {...viewport}
+//       mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+//       onViewPortChange={setViewport}
+//     />
+//   );
+// };
+
+import Map from "react-map-gl";
+
+function App() {
   return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewPortChange={setViewport}
+    <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     />
   );
-};
+}
 
 export default App;
