fddc41f55ecf3b1fae9694b75e54d21bf8d38f90
Peter <peter@Peters-Air.lan>
2022-04-27 21:02:08 -0500

    adding pin drop icon

--- client/src/App.js
+++ client/src/App.js
@@ -51,8 +51,21 @@ function App() {
           longitude={entry.longitude}
           latitude={entry.latitude}
           anchor="bottom">
-          <img src="./pin.png" />
-          <p>{entry.title}</p>
+          <svg
+            className="marker"
+            style={{
+              width: "24px",
+              height: `24px`,
+            }}
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="1.5"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+            <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+            <circle cx="12" cy="10" r="3"></circle>
+          </svg>
         </Marker>
       ))}
     </Map>
